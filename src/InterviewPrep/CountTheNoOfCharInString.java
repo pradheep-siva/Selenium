e32359db1b3e336e9d5763ce08a50345bf1f9c0c
pradheep-siva <pradheepsivakumr848@gmail.com>
2024-08-20 08:46:11 +0530

    Implementation can be provided later

--- src/InterviewPrep/CountTheNoOfCharInString.java
+++ src/InterviewPrep/CountTheNoOfCharInString.java
@@ -4,11 +4,8 @@ import java.util.*;
 public class CountTheNoOfCharInString {
 
 	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-		String name ="";
-		name=sc.next();
-		System.out.println("Enter String name : ");
 		
+		System.out.println("Enter String");
 
 	}
 
