726ecc78d73d6318604fa13be538e3eac419dbd9
pradheep-siva <pradheepsivakumar848@gmail.com>
2024-08-20 08:41:36 +0530

    Update CountTheNoOfCharInString.java

--- src/InterviewPrep/CountTheNoOfCharInString.java
+++ src/InterviewPrep/CountTheNoOfCharInString.java
@@ -1,9 +1,14 @@
 package InterviewPrep;
+import java.util.*;
 
 public class CountTheNoOfCharInString {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		String name ="";
+		name=sc.next();
+		System.out.println("Enter String name : ");
+		
 
 	}
 
